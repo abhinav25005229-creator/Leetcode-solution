80f7d415b47d32574527899f64433cfe6673fc5d
Abhinav Kumar <abhinav.25005229@kiet.edu>
2026-07-04 20:23:49 +0530

    Time: 21 ms (72.19%), Space: 23.5 MB (65.75%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,13 +1,30 @@
-
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root->val==p->val||root->val==q->val)return root;
-        else if(root->val<p->val && root->val>q->val)return root;
-        else if(root->val>p->val && root->val<q->val)return root;
-        else if(root->val<p->val && root->val <q->val)
-        return lowestCommonAncestor(root->right, p, q);
-        else return lowestCommonAncestor(root->left, p, q);
 
+        //////////////  Agar root hi p ya q hai, to wahi LCA hoga.
+        if(root->val == p->val || root->val == q->val)
+            return root;
+
+        ////////// p aur q opposite sides me hain.
+        ////////////// (p left me aur q right me)
+        ////////////// Root hi Lowest Common Ancestor hai.
+        else if(root->val < p->val && root->val > q->val)
+            return root;
+
+        //////////////// q left me aur p right me.
+       ////////////////// Root hi Lowest Common Ancestor hai.
+        else if(root->val > p->val && root->val < q->val)
+            return root;
+
+       //////////////////// Dono nodes right subtree me hain.
+        ////////////////// To right subtree me LCA search karo.
+        else if(root->val < p->val && root->val < q->val)
+            return lowestCommonAncestor(root->right, p, q);
+
+       ////////////////// Dono nodes left subtree me hain.
+      ///////////////////To left subtree me LCA search karo.
+        else
+            return lowestCommonAncestor(root->left, p, q);
     }
 };
