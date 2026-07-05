83c466edb865e9490d462f7da37950ea1954d101
Abhinav Kumar <abhinav.25005229@kiet.edu>
2026-07-05 19:06:32 +0530

    Time: 0 ms (100%), Space: 13.7 MB (57.44%) - LeetHub

---
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+public:
+    int dfs(TreeNode * root, int mn, int mx){
+        if(root==NULL)return mx-mn;
+        mx=max(mx,root->val);
+        mn=min(mn,root->val);
+        return  max(dfs(root->left,mn,mx), dfs(root->right,mn,mx));
+    }
+    int maxAncestorDiff(TreeNode* root) {
+        return dfs(root,root->val, root->val);
+    }
+};
