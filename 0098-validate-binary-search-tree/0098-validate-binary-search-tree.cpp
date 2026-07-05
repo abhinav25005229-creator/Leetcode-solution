43de8d896c99c07a81e7c19b0b39b69b39c290b8
Abhinav Kumar <abhinav.25005229@kiet.edu>
2026-07-05 19:39:38 +0530

    Time: 0 ms (100%), Space: 22.2 MB (8.35%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,13 +1,19 @@
-
+//////////// method 2:
 class Solution {
 public:
-    bool validate(TreeNode * root, long long max, long long min){
-        if(root==NULL)return true;
-        ///// out of range conditiion
-        if(root->val>=max || root->val<=min)return false;
-        return validate(root->left, root->val,min)&& validate(root->right,max, root->val);
+///////////// key point  -> inorder traversal of bst is sorted
+    void inorder(TreeNode * root, vector<int> & arr){
+        if(root==NULL)return;
+        inorder(root->left, arr);
+        arr.push_back(root->val);
+        inorder(root->right, arr);
     }
     bool isValidBST(TreeNode* root) {
-        return validate(root,LLONG_MAX,LLONG_MIN);
+        vector<int>ans;
+        inorder(root,ans);
+        for(int i=1; i<ans.size(); i++){
+            if(ans[i]<=ans[i-1])return false;
+        }
+        return true;
     }
 };
