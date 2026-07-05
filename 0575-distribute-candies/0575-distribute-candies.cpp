12f628a29024a90b4778aa929280274536fe027c
Abhinav Kumar <abhinav.25005229@kiet.edu>
2026-07-05 15:28:02 +0530

    Time: 91 ms (76.46%), Space: 120.1 MB (59.71%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candy) {
-        unordered_set<int>s;
-        for(int x:candy)s.insert(x);
-        int type=s.size();
-        int n=candy.size();
-        return min(n/2,type);
+        unordered_set<int> s(candy.begin(), candy.end());
+            return min((int)s.size(), (int)candy.size()/2);
     }
 };
