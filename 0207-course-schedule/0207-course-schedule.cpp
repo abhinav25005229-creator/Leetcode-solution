2f4013ce89a7a14f02dc14300f7f1e691406f2a7
Abhinav Kumar <abhinav.25005229@kiet.edu>
2026-08-16 19:35:43 +0530

    Time: 7 ms (45.41%), Space: 19.4 MB (48.47%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-bool iscycleDFS(int src, vector<bool>&vis,vector<bool> & recpath,vector<vector<int>>& prerequisites ){
+bool iscycleDFS(int src, vector<bool>&vis,vector<bool> & recpath,vector<vector<int>>& graph ){
     vis[src]=true;
     recpath[src]=true;
-    for(int i=0; i<prerequisites.size(); i++){
-        int v=prerequisites[i][0];
-        int u=prerequisites[i][1];
-        /// checking for neighbours 
-        if(u==src){
+    for(int v: graph[src]){
             if(!vis[v]){
-                if(iscycleDFS(v,vis,recpath,prerequisites))return true;
+                if(iscycleDFS(v,vis,recpath,graph))return true;
             }else if(recpath[v])return true;
         }
        
-         }
+         
           //// backedge 
         recpath[src]=false;
         return false;
 }
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>graph(n);
+        for(auto &p: prerequisites){
+            int course = p[0];
+            int prereq=p[1];
+            graph[prereq].push_back(course);
+        }
+
         vector<bool>vis(n,false);
         vector<bool>recpath(n,false);
         for(int i=0; i<n; i++){
             if(!vis[i]){
-                if(iscycleDFS(i,vis, recpath,prerequisites))return false;
+                if(iscycleDFS(i,vis, recpath,graph))return false;
             }
         }
         return true;
