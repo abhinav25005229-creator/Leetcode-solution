476f4b6f6a2daf681fee7a53e3650bc932158b8e
Abhinav Kumar <abhinav.25005229@kiet.edu>
2026-08-25 11:34:58 +0530

    Time: 0 ms (100%), Space: 24.2 MB (67.36%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
+    bool linear(vector<int>& nums, int key){
+        bool ans=false;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==key)ans=true;
+        }
+        return ans;
+    }
     int missingMultiple(vector<int>& nums, int k) {
        bool ans=false;
        unordered_set<int>s;
-       for(int i=0; i<nums.size(); i++){
-        s.insert(nums[i]);
-       }
+      
        int i=0;
        while(ans!=true){
        i++;
-        if(s.find(k*i)==s.end()){
+        if(linear(nums,k*i)!=true){
             ans=true;
         }
         
