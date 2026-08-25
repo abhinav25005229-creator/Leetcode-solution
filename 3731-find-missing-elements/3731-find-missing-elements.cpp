af503e83a9a5d9c55740fb04adce4482d4e4298f
Abhinav Kumar <abhinav.25005229@kiet.edu>
2026-08-25 19:14:38 +0530

    Time: 6 ms (24.64%), Space: 32.2 MB (91.49%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
+bool linear(vector<int> &arr,int key){
+    bool ans=false;
+    for(int i=0; i<arr.size(); i++){
+        if(arr[i]==key){
+            ans=true;
+        }
+    }
+    return ans;
+}
     vector<int> findMissingElements(vector<int>& nums) {
         vector<int>ans;int n= nums.size();
         sort(nums.begin(),nums.end());
         int small = nums[0];int large= nums[n-1];
 
-        unordered_set<int>s;
-        for(int x:nums)s.insert(x);
-
         for(int i=small; i<=large; i++){
-            if(s.find(i)==s.end())ans.push_back(i);
+           if(linear(nums,i)==false)ans.push_back(i);
         }
         return ans;
     }
