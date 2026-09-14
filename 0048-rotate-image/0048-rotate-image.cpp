4d2ccbe42df7d5675f95361bad26f877fc3dd191
Abhinav Kumar <abhinav.25005229@kiet.edu>
2026-09-14 22:04:10 +0530

    Time: 0 ms (100%), Space: 10.3 MB (36.14%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,17 +4,17 @@ public:
         int n= matrix.size();
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
-                swap(matrix[i][j],matrix[j][i]);
+                swap(matrix[i][j],matrix[j][i]);///// transpose 
             }
         }
         for(int i=0; i<n; i++){
-            int low=0; 
-            int high = n-1;
+            int low=0;
+            int high=n-1;
             while(low<high){
-                int temp =matrix[i][low];
-                matrix[i][low]=matrix[i][high];
+                int temp = matrix[i][low];
+                matrix[i][low] = matrix[i][high];
                 matrix[i][high] = temp;
-                low++; high--;
+                low++;high--;
             }
         }
     }
